6a4df54e46c4240284ca7bbaf6145ae12e35603b
Stan <you@example.com>
2019-12-24 19:01:09 +0100

    Updates

---
+++ precache-manifest.0fbf87748e610307f3fbe278f73f9d25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f33603e5b4ff9a2b309ad01bc833ae",
+    "url": "/react2020-monsters/index.html"
+  },
+  {
+    "revision": "041e40b04c0eb6da0028",
+    "url": "/react2020-monsters/static/css/main.38078d19.chunk.css"
+  },
+  {
+    "revision": "d392583b04fbbf82cf29",
+    "url": "/react2020-monsters/static/js/2.5623ebb2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react2020-monsters/static/js/2.5623ebb2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "041e40b04c0eb6da0028",
+    "url": "/react2020-monsters/static/js/main.763ea523.chunk.js"
+  },
+  {
+    "revision": "27068dc2b9774fa4ac7e",
+    "url": "/react2020-monsters/static/js/runtime-main.564bf5a5.js"
+  }
+]);
